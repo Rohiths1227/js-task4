8c8483552502b1f554653b5193974630b3b00100
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-10 11:06:21 +0530

    Add files via upload

---
+++ IIFE.js
@@ -0,0 +1,12 @@
+//Website Loaded
+(function(){
+    console.log("Website Loaded");
+})();
+
+
+//Discount price
+(function(){
+    let price = 25000;
+    let discount = price * 0.1;
+    console.log("Discount Price:", price-discount);
+})();
