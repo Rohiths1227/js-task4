fd632cdd0f840f80c842ea1adfd9b4155345cce0
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-10 10:55:29 +0530

    Add files via upload

---
+++ Hoisting.js
@@ -0,0 +1,16 @@
+//var hoisting
+console.log(x);
+var x = 10;
+
+
+//let hoisting
+
+let y = 20;
+
+
+//function hoisting
+hello();
+
+function hello(){
+    console.log("Function Hoisting Example");
+}
