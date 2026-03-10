2553bda59e13db5c2f74928a8db9826eff3ee85f
Rohiths1227 <rohithofficial1227@gmail.com>
2026-03-10 11:17:29 +0530

    Add files via upload

---
+++ Array_Manipulation.js
@@ -0,0 +1,17 @@
+//push pop shift unshift
+let arr = [1,2,3,4,5];
+
+arr.push(6);
+arr.pop();
+arr.shift();
+arr.unshift(0);
+
+console.log(arr);
+
+
+//splice example
+let numbers = [1,2,3,40,50,6];
+
+numbers.splice(3,2,4,5);
+
+console.log(numbers);
